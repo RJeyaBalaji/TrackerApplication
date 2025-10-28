6438b85e89e6c319f1acc58b7a11a002cd620740
JeyaBalaji.R <rjeyabalajirjb10@gmail.com>
2025-10-28 12:33:35 +0530

    modified the schedule for testing

--- reports/src/main/java/com/dailystatus/dailyupdate/scheduler/ReportScheduler.java
+++ reports/src/main/java/com/dailystatus/dailyupdate/scheduler/ReportScheduler.java
@@ -23,7 +23,7 @@ public class ReportScheduler {
         this.reportService = reportService;
     }
 
-    @Scheduled(cron = "0 59 11 * * *", zone = "Asia/Kolkata")
+    @Scheduled(cron = "0 38 12 * * *", zone = "Asia/Kolkata")
     public void importPlannedHours() {
         log.info("Morning Excel import");
         runExcelImport();
